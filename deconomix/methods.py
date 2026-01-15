5aebb38d0b83074581f4707ec5979faddefde4ed
a.menschingbuhr <a.menschingbuhr@stud.uni-goettingen.de>
2026-01-15 09:38:09 +0000

    removed all tqdm.notebook calls, replaced them with classic tqdm

--- deconomix/methods.py
+++ deconomix/methods.py
@@ -16,27 +16,7 @@ import matplotlib.ticker as ticker
 #warnings.filterwarnings("ignore", category=UserWarning)
 import scipy.sparse as sp
 import sys
-
-
-# Tqdm based on session:
-def in_notebook():
-    try:
-        # Check if the Jupyter notebook environment exists
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True   # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (maybe an IDE)
-    except NameError:
-        return False  # Probably standard Python interpreter
-
-# Use the result of the check to determine which tqdm to import
-if in_notebook():
-    from tqdm.notebook import tqdm
-else:
-    from tqdm import tqdm
+from tqdm import tqdm
 
 #### DTD #########################################################################################
 
